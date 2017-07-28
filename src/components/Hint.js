a0d9a063800febda60bb5449716910f6fb68dbb9
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2017-07-28 20:04:17 +0100

    Pass props properly

--- src/components/Hint.js
+++ src/components/Hint.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default (destination, duration) => 
+export default ({destination, duration}) => 
     !destination ? <div className="hint">Choose your destination</div>
         : !duration ? <div className="hint">Choose your duration</div>
         : null;
