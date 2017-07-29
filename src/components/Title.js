b603b11403b82b9e432c62c4e257e77d53547f76
Max Cherniavskyi <max.chernyavskyi@akqa.com>
2017-07-29 16:31:58 +0100

    Task 3

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,46 @@ import React, { Component } from 'react';
 class Title extends Component {
     render() {
         return(
-            <h1>{this.props.text}</h1>
+            /*
+             * Task 3. Display Spaceport as a title of the app
+             *
+             * Topics: JSX props. Help: https://facebook.github.io/react/docs/components-and-props.html#rendering-a-component
+             *
+             * TODO: Replace div with h1 and render prop text inside of it.
+             */
+            <div>Title</div>
         );
     }
 }
 
 export default Title;
 
+
+
+
+
+
+
+
+// Hint to Task 3: props are stored in this.props object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Solution to Task 3: <h1>{this.props.text}</h1>
--- src/components/__tests__/Title.test.js
+++ src/components/__tests__/Title.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Title from '../Title';
 
-describe('Task 1', () => {
-    const wrapper = shallow(<Title text="Text"/>);
+describe('Task 3', () => {
 
-    it('Component Title should render a single h1 tag with the value of prop "text"', () => {
-        expect(wrapper.find('h1').length).toBe(1);
-        expect(wrapper.find('h1').text()).toBe('Text');
+    it(`Spaceport should be the title of the app.
+        Dashboard has a Title component with the text prop passed to it.
+        Read the title from the prop text and place it in <h1>.
+        Makes changes at ./src/components/Title.js`, () => {
+        const wrapper = mount(<Title text="Spaceport"/>);
+        expect(wrapper).toHaveHTML('<h1>Spaceport</h1>');
+        const wrapper1 = mount(<Title text="Runway"/>);
+        expect(wrapper1).toHaveHTML('<h1>Runway</h1>');
     });
 })
